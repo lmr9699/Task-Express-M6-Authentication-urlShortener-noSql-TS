61f64bbbeaea1ea17cfbbde6856af020779a2174
lmr9699 <alostad1994@gmail.com>
2026-01-21 23:50:56 +0300

    done

--- src/apis/users/users.controllers.ts
+++ src/apis/users/users.controllers.ts
@@ -1,10 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../../models/User";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const SALT=10
 
 export const signup = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const newUser = await User.create(req.body);
-        res.status(201).json(newUser);
+        const { username, password } = req.body
+
+        const userFound = await User.findOne({ username: username })
+        if (userFound){
+            res.status(400).json({err:"Account already exists"})
+            return
+        }
+        const hashedPassword = await bcrypt.hash(password, SALT)
+
+        const newUser = await User.create({username: username, password: hashedPassword})
+
+        const token = jwt.sign({username: newUser.username, id: newUser._id, role:"admin"},
+             process.env.JWT_SECRET || "", {
+            expiresIn: "7h"
+        })
+        res.status(201).json({token: token});
     } catch (err) {
         next(err);
     }
